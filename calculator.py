93d2f834c58cb83e254e76cff7ac1f10f35014ee
Keith Erwin Mikhail <keitherwinmikhail@Keiths-MacBook-Air.local>
2026-04-20 18:50:54 +0800

    improved calculator

--- calculator.py
+++ calculator.py
@@ -1,8 +1,21 @@
 def main():
-    x = int(input("Whats x? "))
-    print("x squared is", square(x))
+    firstNum = int(input("Input first number: "))
+    secondNum = int(input("Input second number: "))
+    operator = input("Enter operator (+, -, *, /): ")
     
-def square(num):
-    return pow(num, 2)
-
+    if operator == "+":
+        print(f"{firstNum + secondNum}")
+    elif operator == "-":
+        print(f"{firstNum - secondNum}")
+    elif operator == "*":
+        print(f"{firstNum * secondNum}")
+    elif operator == "/":
+        if secondNum != 0:
+            print(f"{firstNum / secondNum}")
+        else:
+            print("Error: Cannot divide by zero")
+    else:
+        print("Invalid operator")
+        
 main()
+        
