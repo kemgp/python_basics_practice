b34562ddc8ea91062fc07b18ab595c08595310d2
Keith Erwin Mikhail <keitherwinmikhail@Keiths-MacBook-Air.local>
2026-04-20 20:49:56 +0800

    added loop to calculator

--- calculator.py
+++ calculator.py
@@ -1,21 +1,34 @@
 def main():
-    firstNum = int(input("Input first number: "))
-    secondNum = int(input("Input second number: "))
-    operator = input("Enter operator (+, -, *, /): ")
-    
-    if operator == "+":
-        print(f"{firstNum + secondNum}")
-    elif operator == "-":
-        print(f"{firstNum - secondNum}")
-    elif operator == "*":
-        print(f"{firstNum * secondNum}")
-    elif operator == "/":
-        if secondNum != 0:
-            print(f"{firstNum / secondNum}")
-        else:
-            print("Error: Cannot divide by zero")
-    else:
-        print("Invalid operator")
+    calculate()
         
+
+def calculate():
+    continueCalculation = True
+    while continueCalculation == True:
+        firstNum = int(input("Input first number: "))
+        secondNum = int(input("Input second number: "))
+        operator = input("Enter operator (+, -, *, /): ")
+        
+        if operator == "+":
+            print(f"{firstNum + secondNum}")
+        elif operator == "-":
+            print(f"{firstNum - secondNum}")
+        elif operator == "*":
+            print(f"{firstNum * secondNum}")
+        elif operator == "/":
+            if secondNum != 0:
+                print(f"{firstNum / secondNum}")
+            else:
+                print("Error: Cannot divide by zero")
+        else:
+            print("Invalid operator")
+            
+        next_calculation = input("Do you want to calculate again? (Yes/No)")
+        if next_calculation == "No":
+            continueCalculation = False
+    
+
+
+
 main()
         
--- conditionals.py
+++ conditionals.py
@@ -1,1 +1,12 @@
 #practicing conditionals
+
+#comparison
+num1 = int(input("What is the first number? "))
+num2 = int(input("What is the second number? "))
+
+if num1 < num2:
+    print("The first number is less than the second number")
+elif num1 > num2:
+    print("The first number is greater than the second number")
+elif num1 == num2:
+    print("The two numbers are equal")
