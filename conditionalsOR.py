e183ba3aa9fd5daa397eedf579cd69acd15c6f0f
Keith Erwin Mikhail <keitherwinmikhail@Keiths-MacBook-Air.local>
2026-04-21 16:00:40 +0800

    added notes

--- conditionalsOR.py
+++ conditionalsOR.py
@@ -1,6 +1,8 @@
 x = int(input("Enter the first number: "))
 y = int(input("Enter the second number: "))
 
+#originally used if x > y or x < y, but this is more efficient because it only checks if x and y are not equal, which is the only condition that matters when checking for equality
+
 if x != y:
     print("The two numbers are not equal")
 else:
