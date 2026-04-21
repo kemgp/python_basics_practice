d739579c2e77e3c5d98ab26ba67afc4ada66bbff
Keith Erwin Mikhail <keitherwinmikhail@Keiths-MacBook-Air.local>
2026-04-21 15:48:07 +0800

    conditionals

--- conditionals.py
+++ conditionals.py
@@ -1,12 +1,13 @@
 #practicing conditionals
 
 #comparison
-num1 = int(input("What is the first number? "))
-num2 = int(input("What is the second number? "))
+def comparison():
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
 
-if num1 < num2:
-    print("The first number is less than the second number")
-elif num1 > num2:
-    print("The first number is greater than the second number")
-elif num1 == num2:
-    print("The two numbers are equal")
+    if num1 < num2:
+        print("The first number is less than the second number")
+    elif num1 > num2:
+        print("The first number is greater than the second number")
+    else:
+        print("The two numbers are equal")
