ad7b3032777f621f3fca43faeb6739db0b7130c5
Keith Erwin Mikhail <keitherwinmikhail@Keiths-MacBook-Air.local>
2026-04-21 15:53:49 +0800

    conditionals or

--- conditionals.py
+++ conditionals.py
@@ -9,5 +9,7 @@ def comparison():
         print("The first number is less than the second number")
     elif num1 > num2:
         print("The first number is greater than the second number")
-    else:
+    else: #else is used when all other conditions are false, more efficient than using elif num1 == num2
         print("The two numbers are equal")
+        
+comparison()
---
+++ conditionalsOR.py
@@ -0,0 +1,7 @@
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+if x != y:
+    print("The two numbers are not equal")
+else:
+    print("The two numbers are equal")
